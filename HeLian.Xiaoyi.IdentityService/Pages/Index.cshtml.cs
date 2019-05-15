6047341c04471acfbbdefffd78f4c1fd65150066
Martin Zhao <shuijhu@gmail.com>
2019-05-15 14:10:54 +0800

    add project

---
+++ HeLian.Xiaoyi.IdentityService/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeLian.Xiaoyi.IdentityService.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
