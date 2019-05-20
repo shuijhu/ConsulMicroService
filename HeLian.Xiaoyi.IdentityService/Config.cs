3c90c3100ac668a315e863473b3d754d36826f76
Martin Zhao <shuijhu@gmail.com>
2019-05-20 09:07:27 +0800

    indentity config

--- HeLian.Xiaoyi.IdentityService/Config.cs
+++ HeLian.Xiaoyi.IdentityService/Config.cs
@@ -8,11 +8,19 @@ namespace HeLian.Xiaoyi.IdentityService
 {
     public class Config
     {
+        public static IEnumerable<IdentityResource> GetIdentityResourceResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(), //必须要添加，否则报无效的scope错误
+                new IdentityResources.Profile()
+            };
+        }
         public static IEnumerable<ApiResource> GetApiResource()
         {
             var res = new List<ApiResource>();
             res.Add(new ApiResource() { Name = "UserAPI", Description = "用户API" });
-            res.Add(new ApiResource() { Name = "ProjectAPI", Description = "项目API" });
+            //res.Add(new ApiResource() { Name = "ProjectAPI", Description = "项目API" });
 
             return res;
         }
@@ -22,11 +30,13 @@ namespace HeLian.Xiaoyi.IdentityService
             var res = new List<Client>();
             res.Add(new Client()
             {
-                ClientId = "userservice",
+                ClientId = "webapp",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = {new Secret("123987".Sha256()) { } },
                 AllowedScopes = { "UserAPI", "ProjectAPI" },
+                AllowAccessTokensViaBrowser = true
             });
+
 
             return res;
         }
--- HeLian.Xiaoyi.IdentityService/Startup.cs
+++ HeLian.Xiaoyi.IdentityService/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,8 +31,12 @@ namespace HeLian.Xiaoyi.IdentityService
 
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResourceResources())
                 .AddInMemoryApiResources(Config.GetApiResource())
                 .AddInMemoryClients(Config.GetClients());
+            
+                //.AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
+                //.AddProfileService<ProfileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
