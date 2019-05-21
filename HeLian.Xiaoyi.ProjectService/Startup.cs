3db8f4269c922525d2ef39eae0ac8ffb53efa145
Martin Zhao <shuijhu@gmail.com>
2019-05-21 09:11:16 +0800

    add consul, new project

--- HeLian.Xiaoyi.ProjectService/Startup.cs
+++ HeLian.Xiaoyi.ProjectService/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace HeLian.Xiaoyi.UserService
+namespace HeLian.Xiaoyi.ProjectService
 {
     public class Startup
     {
---
+++ HeLian.Xiaoyi.UserService/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeLian.Xiaoyi.UserService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Health")]
+    public class HealthController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("ok");
+    }
+}
